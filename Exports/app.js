ced79be664019292ae374f2114eadb6d102c0f7a
CdubemB <dubemwb@gmail.com>
2019-05-23 21:05:32 +0100

    Added comments to the code

--- Exports/app.js
+++ Exports/app.js
@@ -1,7 +1,7 @@
 //Master Code
 
-const source = require("./source");
+const source = require("./source");  //Getting the source code from the source file
 
-console.log(source.sum(1, 5));
-console.log(source.PI);
-console.log(new source.someMathObj());
+console.log(source.sum(1, 5)); // putting the sum variable in the console
+console.log(source.PI); // putting the PI variable in the console
+console.log(new source.someMathObj()); // putting the someMathObj object into the console
--- Exports/source.js
+++ Exports/source.js
@@ -1,15 +1,15 @@
 //Export 1 source code
 
-const sum = (num1, num2) => num1 + num2;
-const PI = 3.14;
+const sum = (num1, num2) => num1 + num2; //This i=will mae a sum variable with 2 parameters which will be added together
+const PI = 3.14; //this creates a variable which represents PI
 class someMathObj {
     constructor() {
         console.log("Object has been created")
     }
-}
+} //This creates a constructor for an object
 
 module.exports = {
     sum: sum,
     PI: PI,
     someMathObj: someMathObj
-}
+} //This shows how these variables are being exported 
