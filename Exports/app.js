4554ce91cfb35b3b88bbeff4f53ce47e93a76bd6
CdubemB <dubemwb@gmail.com>
2019-06-17 18:05:41 +0100

    have added the main function and put the output inside it

--- Exports/app.js
+++ Exports/app.js
@@ -1,7 +1,14 @@
+
 //Master Code
 
 const source = require("./source");  //Getting the source code from the source file
 
-console.log(source.sum(1, 5)); // putting the sum variable in the console
-console.log(source.PI); // putting the PI variable in the console
-console.log(new source.someMathObj()); // putting the someMathObj object into the console
+function main() {
+    console.log(source.sum(1, 5)); // putting the sum variable in the console
+    console.log(source.PI); // putting the PI variable in the console
+    console.log(new source.someMathObj()); // putting the someMathObj object into the console
+
+    return 0;
+}
+
+main();
